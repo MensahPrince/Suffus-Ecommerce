19586efd547144e4041eafdb0a92d212c792f3f6
MensahPrince <burnszephyr520@gmail.com>
2025-03-30 22:28:55 +0000

    '

--- stuffsus/models.py
+++ stuffsus/models.py
@@ -1,6 +1,7 @@
 # Custom User Model (for login/logout)
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
+
 # Create your models here.
 
 class User(AbstractUser):
