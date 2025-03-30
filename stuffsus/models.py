8fc7569a26a525fdefd929100f42ad0237645c00
MensahPrince <burnszephyr520@gmail.com>
2025-03-30 22:27:50 +0000

    Add comment to clarify model creation in User model

--- stuffsus/models.py
+++ stuffsus/models.py
@@ -1,6 +1,7 @@
 # Custom User Model (for login/logout)
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
+# Create your models here.
 
 class User(AbstractUser):
     profile_picture = models.ImageField(upload_to='profiles/', null=True, blank=True)
