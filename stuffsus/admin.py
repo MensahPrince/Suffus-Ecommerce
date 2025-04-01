bccac3c49d854b46827e2586479c39ff99c81330
MensahPrince <burnszephyr520@gmail.com>
2025-04-01 22:09:11 +0000

    Refactor admin.py to improve model registration clarity

--- stuffsus/admin.py
+++ stuffsus/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Product, Category, Cart, Order, OrderItem
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Cart)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(User, UserAdmin)
